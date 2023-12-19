9bf08406d71f819640a93272c12721bb950be23a
Gorynch <i231081@yandex.ru>
2023-12-19 12:56:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HomeWork about decorator in Python by Igor Golovin
+
+Netology 2023 Dec
---
+++ main_01.py
@@ -0,0 +1,59 @@
+'''Доработать декоратор logger в коде ниже. Должен получиться декоратор,
+который записывает в файл 'main.log' дату и время вызова функции, имя функции,
+аргументы, с которыми вызвалась, и возвращаемое значение.
+Функция test_1 в коде ниже также должна отработать без ошибок'''
+
+log_file_path = 'main.log'
+
+import os, functools
+from datetime import datetime
+
+def logger(old_function):
+    @functools.wraps(old_function)
+    def wrapper(*args, **kwargs):
+        log_string = f'{datetime.now().date()},{datetime.now().time()},{old_function.__name__},{args},{kwargs},{old_function(*args,**kwargs)}\n'
+        with open(log_file_path, 'a') as f:
+            f.write(log_string)
+        return old_function(*args, **kwargs)
+    return wrapper
+
+def test_1():
+    path = 'main.log'
+    if os.path.exists(path):
+        os.remove(path)
+
+    @logger
+    def hello_world():
+        return 'Hello World'
+
+    @logger
+    def summator(a, b=0):
+        return a + b
+
+    @logger
+    def div(a, b):
+        return a / b
+
+    assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
+    result = summator(2, 2)
+    assert isinstance(result, int), 'Должно вернуться целое число'
+    assert result == 4, '2 + 2 = 4'
+    result = div(6, 2)
+    assert result == 3, '6 / 2 = 3'
+
+    assert os.path.exists(path), 'файл main.log должен существовать'
+
+    summator(4.3, b=2.2)
+    summator(a=0, b=0)
+
+    with open(path) as log_file:
+        log_file_content = log_file.read()
+
+    assert 'summator' in log_file_content, 'должно записаться имя функции'
+    for item in (4.3, 2.2, 6.5):
+        assert str(
+            item) in log_file_content, f'{item} должен быть записан в файл'
+
+
+if __name__ == '__main__':
+    test_1()
